3d4af1b07604e4947482f36dd2487c7930fee281
shruthishruu05 <shruthi.m@perfios.com>
2021-09-06 09:31:23 +0530

    REFACTORED : added show method

--- src/com/bridgelabz/tictactoe/TicTacToeBoard.java
+++ src/com/bridgelabz/tictactoe/TicTacToeBoard.java
@@ -17,10 +17,13 @@ public class TicTacToeBoard
 	}
 	public void showBoard() {
 		
-		for(int index=1;index<=10;index++) {
-				System.out.print(board[index]+" ");
-			
-			System.out.println();
+		System.out.println("Board");
+		for(int rowIndex=0; rowIndex<3; rowIndex++) {
+			for(int colIndex=0; colIndex<3; colIndex++) {
+				System.out.print(" 0 ");
+			}
+			System.out.print("\n");
+			System.out.println("");
 		}
 	}
 	public char InputChoice() {
