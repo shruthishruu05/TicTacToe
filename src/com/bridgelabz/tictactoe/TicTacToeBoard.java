f14ec2c54945d363889dff82daa3ab539229c6ff
shruthishruu05 <shruthi.m@perfios.com>
2021-09-06 17:30:28 +0530

    ADDED : inorder to move to a desired location

--- src/com/bridgelabz/tictactoe/TicTacToeBoard.java
+++ src/com/bridgelabz/tictactoe/TicTacToeBoard.java
@@ -1,38 +1,49 @@
 package com.bridgelabz.tictactoe;
-
 import java.util.Scanner;
 
 public class TicTacToeBoard 
 {
+	static char computerInput,humanInput;
 	static Scanner scanner = new Scanner(System.in);
-	static char[] board = new char[11];
-	public void start() {
+	static char[] board = new char[10];
+	public static void createBoard() {
 		
-		for(int index=1;index<=10;index++) {
+		for(int index=1;index<10;index++) {
 				board[index] = ' ';
 		}
 		System.out.println("Board is this :");
 		showBoard();
 		
 	}
-	public void showBoard() {
+	public static void moveDesiredLocation()
+	{
+		System.out.println("select between 1 to 9 to make a move to a desired location");
+		int desiredLocation = scanner.nextInt();
 		
-		System.out.println("Board");
-		for(int rowIndex=0; rowIndex<3; rowIndex++) {
-			for(int colIndex=0; colIndex<3; colIndex++) {
-				System.out.print(" 0 ");
+	}
+	public static void showBoard() {
+		System.out.println("Board representation");
+		for(int index1=0; index1<3; index1++) {
+			for(int index2=0; index2<3; index2++) {
+				System.out.print(board[index1+index2+1]+" | ");
 			}
 			System.out.print("\n");
 			System.out.println("");
 		}
 	}
-	public char InputChoice() {
+	public static void playerInputChoice() {
 		
 		System.out.println("Enter letter: ");
-		return scanner.next().toUpperCase().charAt(0);
+	char choice =  scanner.next().toUpperCase().charAt(0);
+		char result = (computerInput =='X')? 'X':'0';
 		
+	}
+	
+	public static void main(String [] args) {
+		playerInputChoice();
+		createBoard();
+		moveDesiredLocation();
 		
 	}
 }
-	
 
