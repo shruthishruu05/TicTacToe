2be5a34b51e7b1894ac079b8d88d6b764a283554
shruthishruu05 <shruthi.m@perfios.com>
2021-09-06 22:15:46 +0530

    REFACTORED : class to determine if game won lost or tie

--- src/com/bridgelabz/tictactoe/TicTacToeBoard.java
+++ src/com/bridgelabz/tictactoe/TicTacToeBoard.java
@@ -1,12 +1,15 @@
 package com.bridgelabz.tictactoe;
 import java.util.Scanner;
-
 public class TicTacToeBoard 
 {
-	static char computerInput,humanInput,first;
+	static char computerInput,humanInput,first,value,human,computer;
 	static Scanner scanner = new Scanner(System.in);
 	static char[] board = new char[10];
 	static int desiredLocation;
+	static boolean result;
+	private static boolean humanInputToss;
+	private static boolean win = false;
+	
 	public static void createBoard() 
 	{
 		
@@ -17,6 +20,91 @@ public class TicTacToeBoard
 		showBoard();
 		
 	}
+		
+	public static void humanMove()
+	{
+		//System.out.println("human turn");
+		System.out.println("\nEnter Location 1-9 to Make Move");
+
+		while (true) {
+
+			desiredLocation = scanner.nextInt();
+			if (checkIfEmpty(desiredLocation) && desiredLocation < 10 && desiredLocation > 0) {
+				System.out.println("Enter the human input");
+				board[desiredLocation] = humanInput;
+				showBoard();
+				break;
+			}
+			else 
+			{
+				System.out.println("Invalid Choice");
+
+			}
+		}
+	}
+	public static void computerMove() {
+		do {
+			System.out.println("Computer turn");
+			desiredLocation = 	(int)(Math.floor(Math.random()*10)%9) + 1;								
+		} while (!checkIfEmpty(desiredLocation));
+
+		board[desiredLocation] = computerInput;
+		showBoard();
+	}
+	
+	public static boolean checkWinner() {
+		if (win)
+			return true;
+		 char h = humanInput;
+		 char c = computerInput;
+		
+		if ((board[1] == h && board[2] == h && board[3] == h)|| (board[4] == h && board[5] == h && board[6] == h)
+				|| (board[7] == h && board[8] == h && board[9] == h)|| (board[1] == h && board[4] == h && board[7] == h)
+				|| (board[2] == h && board[5] == h && board[8] == h)|| (board[3] == h && board[6] == h && board[9] == h)
+				|| (board[1] == h && board[5] == h && board[9] == h)|| (board[3] == h && board[5] == h && board[7] == h))
+			{
+			System.out.println("human won");
+			win = true;
+			return true;
+		}
+
+		if ((board[1] == c && board[2] == c && board[3] == c)|| (board[4] == c && board[5] == c && board[6] == c)
+				|| (board[7] == c && board[8] == c && board[9] == c)|| (board[1] == c && board[4] == c && board[7] == c)	
+				|| (board[2] == c && board[5] == c && board[8] == c)|| (board[3] == c && board[6] == c && board[9] == c)
+				|| (board[1] == c && board[5] == c && board[9] == c)|| (board[3] == c && board[5] == c && board[7] == c)) 
+				{
+				System.out.println("Computer won");
+				win = true;
+				return true;
+		}
+
+		return false;
+	}
+	public static boolean isFull() {
+		if ((board[1] != ' ') && (board[2] != ' ') && (board[3] != ' ') && (board[4] != ' ') && (board[5] != ' ')
+				&& (board[6] != ' ') && (board[7] != ' ') && (board[8] != ' ') && (board[9] != ' ')) {
+
+			return true;
+		}
+		return false;
+	}
+	public static void start() 
+	{
+		do {
+			if (humanInputToss) {
+				humanMove();
+				if (!isFull())
+					computerMove();
+			} else {
+				computerMove();
+				if (!isFull())
+					humanMove();
+			}
+		} while (!checkWinner() && !isFull());
+		if (isFull() && !checkWinner())
+			System.out.println("tied");
+
+	}
 	public static boolean checkIfEmpty(int desiredLocation)
 	{	
 		if(board[desiredLocation]== ' ')
@@ -24,21 +112,8 @@ public class TicTacToeBoard
 			return true;
 		}
 		else 
-				System.out.println("canot make a move as empty space is not available");
 		return false;
 		
-		
-	}
-	private static void moveDesiredLocation(char value,boolean result) 
-	{
-		if(result == true)
-		{
-		board[desiredLocation] = value;
-		showBoard();
-		}
-		else {
-			System.out.println("location occupied");
-		}
 		
 	}
 	public static void showBoard() {
@@ -51,41 +126,45 @@ public class TicTacToeBoard
 		}
 		
 	}
-	public static void playerInputChoice() {
+	public static void playerInputChoice() 
+	{
 		
-		System.out.println("Enter letter: ");
-	char choice =  scanner.next().toUpperCase().charAt(0);
-		char result = (computerInput =='X')? 'X':'0';
-		
+		System.out.print("select X or O : ");
+		humanInput = scanner.next().toUpperCase().charAt(0);
+
+		if (humanInput == 'X')
+			computerInput = 'O';
+		else
+			computerInput = 'X';
+		System.out.println("human has selected : " + humanInput);
+		System.out.println("Computer choice is : " + computerInput);
+	
 	}
+	
 	public static void checkWhoPlays()
 	{
 		System.out.println("Tossing");
 		String first;
 
 		if (Math.random() > 0.5)
-			first = "c";
+			first = "computer";
 		else
-			first = "h";
+			first = "human";
 
-		if (first.equals("c")) {
+		if (first.equals("computer")) {
 			System.out.println("Computer goes first");
 		}
 		else {
 			System.out.println("human goes first");
-		}
+		}	
 	}
 	public static void main(String [] args) {
-		playerInputChoice();
 		createBoard();
-		System.out.println("Enter a desired location");
-		desiredLocation = scanner.nextInt();
-		boolean result = checkIfEmpty(desiredLocation);
-		System.out.println("Enter the value");
-		char value = scanner.next().charAt(0);
-		moveDesiredLocation(value,result);
+		playerInputChoice();
 		checkWhoPlays();
+		//showBoard();
+		start();
+
 	}
-	
 }
 
